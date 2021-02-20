5f2d399b5422436533a14174014eee2463a3b0e8
Anyel Martich <anyel.martich@gmail.com>
2021-02-20 22:32:27 +0100

    Update numeros.ps1

--- PowerShell/numeros.ps1
+++ PowerShell/numeros.ps1
@@ -1,1 +1,12 @@
 
+$numero = Read-Host "Digite un numero"
+$numero = [math]::Pow($numero , $numero)
+
+$division = $numero % 2
+
+if($division -eq 0 ){
+    write "El numero es par"
+
+}else{
+    write "El numero es impar"
+}
